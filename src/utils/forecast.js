2c1342bbb9f7f2f0cc3fe16952c1805a83513c58
Ömer Dedeoğlu <omer5288@gmail.com>
2020-10-11 19:57:03 +0300

    add humidity rate

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
    callback('Unable to find location', undefined)
   }
   else {
-   callback(undefined, body.current.weather_descriptions + '. It is curently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out.')
+   callback(undefined, body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%.')
   }
  })
 }
